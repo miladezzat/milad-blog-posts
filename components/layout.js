ff18a1353c26ca2ed4350b7aa5295519aaee3186
Milad E. Fahmy <miladezzat.f@gmail.com>
2021-06-19 20:04:50 +0200

    google analytics

--- components/layout.js
+++ components/layout.js
@@ -21,12 +21,16 @@ export default function Layout({ children, home }) {
         <meta property="og:image" content="/images/profile.png" />
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="/images/profile.png" />
-        <div dangerouslySetInnerHTML={{ __html: `<script>
-  !function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware"];analytics.factory=function(e){return function(){var t=Array.prototype.slice.call(arguments);t.unshift(e);analytics.push(t);return analytics}};for(var e=0;e<analytics.methods.length;e++){var key=analytics.methods[e];analytics[key]=analytics.factory(key)}analytics.load=function(key,e){var t=document.createElement("script");t.type="text/javascript";t.async=!0;t.src="https://cdn.segment.com/analytics.js/v1/" + key + "/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n);analytics._loadOptions=e};analytics._writeKey="ZQeiscAhvET7lmyu8Dm8RJX8n4aag6sF";analytics.SNIPPET_VERSION="4.13.2";
-  analytics.load("ZQeiscAhvET7lmyu8Dm8RJX8n4aag6sF");
-  analytics.page();
-  }}();
-</script>` }} />
+        <div dangerouslySetInnerHTML={{ __html: `
+          <!-- Global site tag (gtag.js) - Google Analytics -->
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-XZ2KJXR4P6"></script>
+          <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag() { dataLayer.push(arguments); }
+            gtag('js', new Date());
+        
+            gtag('config', 'G-XZ2KJXR4P6');
+          </script>` }} />
       </Head>
       <header className={styles.header}>
         {home ? (

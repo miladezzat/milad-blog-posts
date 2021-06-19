8770eaa8450ee17520cb205b1a326f2ed436f7fc
Milad E. Fahmy <miladezzat.f@gmail.com>
2021-06-19 19:51:36 +0200

    google anyltics

--- components/layout.js
+++ components/layout.js
@@ -18,9 +18,15 @@ export default function Layout({ children, home }) {
         <meta name="author" content="Milad E. Fahmy" />
         <meta property="og:description" content="Milad Ezzat, A Software Engineer with industry experience building web services. Specializing in Full Stack development with Node.js, Reactjs." />
         <meta name="twitter:description" content="Milad Ezzat, A Software Engineer with industry experience building web services. Specializing in Full Stack development with Node.js, Reactjs." />
-        <meta property="og:image"content= "/images/profile.png" />
+        <meta property="og:image" content="/images/profile.png" />
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="/images/profile.png" />
+        <div dangerouslySetInnerHTML={{ __html: `<script>
+  !function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware"];analytics.factory=function(e){return function(){var t=Array.prototype.slice.call(arguments);t.unshift(e);analytics.push(t);return analytics}};for(var e=0;e<analytics.methods.length;e++){var key=analytics.methods[e];analytics[key]=analytics.factory(key)}analytics.load=function(key,e){var t=document.createElement("script");t.type="text/javascript";t.async=!0;t.src="https://cdn.segment.com/analytics.js/v1/" + key + "/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n);analytics._loadOptions=e};analytics._writeKey="ZQeiscAhvET7lmyu8Dm8RJX8n4aag6sF";analytics.SNIPPET_VERSION="4.13.2";
+  analytics.load("ZQeiscAhvET7lmyu8Dm8RJX8n4aag6sF");
+  analytics.page();
+  }}();
+</script>` }} />
       </Head>
       <header className={styles.header}>
         {home ? (

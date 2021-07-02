279daf6b0bbd3934592d8a96b4124538e80e71bc
Milad E. Fahmy <miladezzat.f@gmail.com>
2021-07-02 13:28:44 -0400

    updated

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -33,20 +33,6 @@ export default function Post({ postData }) {
                     <div className={utilStyles.lightText}>
                         <Date dateString={postData.created} readingTime={postData.readingTime} />
                     </div>
-                    <div dangerouslySetInnerHTML={{
-                        __html: `
-                    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-                    <!-- articles -->
-                    <ins class="adsbygoogle"
-                        style="display:block"
-                        data-ad-client="ca-pub-7839879913941805"
-                        data-ad-slot="4527175749"
-                        data-ad-format="auto"
-                        data-full-width-responsive="true"></ins>
-                    <script>
-                        (adsbygoogle = window.adsbygoogle || []).push({});
-                    </script>
-                ` }} />
                     <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
                 </article>
             </Layout>

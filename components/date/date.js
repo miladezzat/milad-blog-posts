48086bd1a93be384e60e55816f88acb29fe1bcd1
Milad E. Fahmy <miladezzat.f@gmail.com>
2021-07-03 09:43:07 -0400

    sitemap

---
+++ components/date/date.js
@@ -0,0 +1,10 @@
+import { parseISO, format } from 'date-fns';
+import styles from './date.module.css';
+
+export default function Date({ dateString, readingTime }) {
+    const date = parseISO(dateString);
+
+    return <div dateTime={dateString} className={styles.timeReading}>
+        {format(date, 'LLLL d, yyyy')} &#9716; {readingTime}
+    </div>
+}
---
+++ next-sitemap.js
@@ -0,0 +1,4 @@
+module.exports = {
+  siteUrl: 'https://milad-ezzat.vercel.app',
+  sitemapSize: 7000,
+}

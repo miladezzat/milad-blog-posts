0289b543571cc3cfb829d72beee11a57a7ce67a3
Milad E. Fahmy <miladezzat.f@gmail.com>
2021-07-03 10:24:18 -0400

    posts data

--- pages/index.js
+++ pages/index.js
@@ -3,24 +3,26 @@ import NavBar from '../components/nav-bar/nav-bar';
 import Footer from '../components/footer';
 import Layout from '../components/layout';
 import { getSortedPostsData } from '../lib/posts';
+import { useState } from 'react';
 
-function paginate(array, page_size = 4, page_number = 1) {
-    return array.slice((page_number - 1) * page_size, page_number * page_size);
+function paginate(array, pageSize = 4, pageNumber = 1) {
+    return array.slice((pageNumber - 1) * pageSize, pageNumber * pageSize);
 }
 
 export async function getStaticProps() {
     const allPostsData = getSortedPostsData();
 
-    console.log(paginate(allPostsData));
     return {
         props: {
-            allPostsData
+            allPostsData: paginate(allPostsData),
         }
     }
 }
 
 
-export default function Home() {
+export default function Home({ allPostsData }) {
+    const [pageNumber, setPageNumber] = useState(1);
+
     return (
         <Layout>
             <Head>
@@ -45,61 +47,38 @@ export default function Home() {
             <div className="container px-4 px-lg-5">
                 <div className="row gx-4 gx-lg-5 justify-content-center">
                     <div className="col-md-10 col-lg-8 col-xl-7">
-                        {/* <!-- Post preview--> */}
-                        <div className="post-preview">
-                            <a href="post.html">
-                                <h2 className="post-title">Man must explore, and this is exploration at its greatest</h2>
-                                <h3 className="post-subtitle">Problems look mighty small from 150 miles up</h3>
-                            </a>
-                            <p className="post-meta">
-                                Posted by
-                                <a href="#!">Start Bootstrap</a>
-                                on September 24, 2021
-                            </p>
+                        {
+                            allPostsData.map(post => {
+                                {/* <!-- Post preview--> */ }
+                                <div className="post-preview">
+                                    <a href="post.html">
+                                        <h2 className="post-title">Man must explore, and this is exploration at its greatest</h2>
+                                        <h3 className="post-subtitle">Problems look mighty small from 150 miles up</h3>
+                                    </a>
+                                    <p className="post-meta">
+                                        Posted by
+                                        <a href="#!">Start Bootstrap</a>
+                                        on September 24, 2021
+                                    </p>
+                                </div>
+                                {/* <!-- Divider--> */ }
+                                <hr className="my-4" />
+                            })
+                        }
+
+
+                        <div className="d-flex justify-content-end  mb-4">
+                            {/* <!-- Pager--> */}
+                            {
+                                pageNumber > 1 &&
+                                <div className=" d-flex justify-content-start w-50"><a className="btn btn-primary text-uppercase" href="#!">←Latest Posts</a></div>
+                            }
+
+
+                            {/* <!-- Pager--> */}
+                            <div className="d-flex justify-content-end w-50"><a className="btn btn-primary text-uppercase" href="#!">Older Posts →</a></div>
                         </div>
-                        {/* <!-- Divider--> */}
-                        <hr className="my-4" />
-                        {/* <!-- Post preview--> */}
-                        <div className="post-preview">
-                            <a href="post.html"><h2 className="post-title">I believe every human has a finite number of heartbeats. I do not intend to waste any of mine.</h2></a>
-                            <p className="post-meta">
-                                Posted by
-                                <a href="#!">Start Bootstrap</a>
-                                on September 18, 2021
-                            </p>
-                        </div>
-                        {/* <!-- Divider--> */}
-                        <hr className="my-4" />
-                        {/* <!-- Post preview--> */}
-                        <div className="post-preview">
-                            <a href="post.html">
-                                <h2 className="post-title">Science has not yet mastered prophecy</h2>
-                                <h3 className="post-subtitle">We predict too much for the next year and yet far too little for the next ten.</h3>
-                            </a>
-                            <p className="post-meta">
-                                Posted by
-                                <a href="#!">Start Bootstrap</a>
-                                on August 24, 2021
-                            </p>
-                        </div>
-                        {/* <!-- Divider--> */}
-                        <hr className="my-4" />
-                        {/* <!-- Post preview--> */}
-                        <div className="post-preview">
-                            <a href="post.html">
-                                <h2 className="post-title">Failure is not an option</h2>
-                                <h3 className="post-subtitle">Many say exploration is part of our destiny, but it’s actually our duty to future generations.</h3>
-                            </a>
-                            <p className="post-meta">
-                                Posted by
-                                <a href="#!">Start Bootstrap</a>
-                                on July 8, 2021
-                            </p>
-                        </div>
-                        {/* <!-- Divider--> */}
-                        <hr className="my-4" />
-                        {/* <!-- Pager--> */}
-                        <div className="d-flex justify-content-end mb-4"><a className="btn btn-primary text-uppercase" href="#!">Older Posts →</a></div>
+
                     </div>
                 </div>
             </div>

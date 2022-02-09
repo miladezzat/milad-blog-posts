9f344148e6809effc0b64a1ef1c7126cf1c87ecd
Milad E. Fahmy <miladezzat.f@gmail.com>
2022-02-09 09:50:33 +0200

    google search comsole

--- pages/_document.js
+++ pages/_document.js
@@ -27,6 +27,10 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
             rel="stylesheet"
           />
+          <meta
+            name="google-site-verification"
+            content="_-p2XdKwtGd6MPXJye2VTAIfwoQtDxLoelu7ojSoNqM"
+          />
           <link
             rel="stylesheet"
             href="https://cdn.jsdelivr.net/npm/katex@0.13.11/dist/katex.min.css"

5d38c6e8e928fd709f0c64f8f74164690da8e698
Milad E. Fahmy <miladezzat.f@gmail.com>
2022-02-24 14:35:16 +0200

    add new course

--- data/coursesData.js
+++ data/coursesData.js
@@ -1,5 +1,14 @@
 const projectsData = [
   {
+    title: 'Node.js',
+    description: `This course is aimed at developers who already have an understanding of JavaScript, MongoDB,Express.js, and APIs. This mini-course is ideal for you if:
+    - You are a busy professional who wants to understand. how you set up a scalable structure for your project.
+    - You want to set up a good validation layer.
+    - You want to understand authentication.`,
+    imgSrc: 'https://miladezzat.github.io/images/node-js.png',
+    href: 'https://miladezzat.github.io',
+  },
+  {
     title: 'Todo App | JavaScript',
     description: `Building A Vanilla JavaScript Todo App From Start To Finish`,
     imgSrc: '/static/courses/javascript.jpg',

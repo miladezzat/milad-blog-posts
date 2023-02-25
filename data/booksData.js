7b3cd91439022cefd76266fe8a9590e17a8a0770
Milad E. Fahmy <miladezzat.f@gmail.com>
2023-02-25 20:22:49 +0200

    add books

---
+++ data/booksData.js
@@ -0,0 +1,16 @@
+const booksData = [
+  {
+    title: "Learning Javascript: Beginner's Guide To Programming",
+    description: `Welcome to the exciting world of Javascript! Whether you're a complete newbie to programming or have some experience with other languages, this book will provide you with a comprehensive and beginner-friendly introduction to the fundamentals of Javascript. With its ubiquitous presence on the web, Javascript has become an essential tool for developers, and in this book, we'll guide you through the basics of programming with Javascript. From data types and variables to functions and objects, we'll help you build a strong foundation in this versatile language, and set you on a path towards creating powerful and interactive web applications. So let's dive in and start your journey to becoming a Javascript programmer!`,
+    imgSrc: '/static/books/Learning-Javascript-Beginner.jpeg',
+    href: 'https://a.co/d/i0uGR3Z',
+  },
+  {
+    title: 'Javascript fundamental: A comprehensive guide to modern development',
+    description: `In this book, we have will cover a wide range of JavaScript concepts and best practices, starting with an introduction to the history, syntax, and rules of the language. We then delved into data types, control flow and loops, functions and objects, arrays and strings, DOM manipulation, asynchronous JavaScript, and error handling and debugging.`,
+    imgSrc: '/static/books/Javascript-fundamental-comprehensive.jpeg',
+    href: 'https://a.co/d/dYm4bkj',
+  },
+]
+
+export default booksData
--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -2,6 +2,7 @@ const headerNavLinks = [
   { href: '/blog', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
   { href: '/projects', title: 'Projects' },
+  { href: '/books', title: 'Books' },
   { href: '/courses', title: 'Courses' },
   { href: '/about', title: 'About' },
 ]

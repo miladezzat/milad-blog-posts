89e049fb10cbc3e63eb27f2c2eb5e0b0d638388c
Milad E. Fahmy <miladezzat.f@gmail.com>
2024-09-03 19:08:16 +0300

    update projects

--- data/projectsData.js
+++ data/projectsData.js
@@ -6,16 +6,16 @@ const projectsData = [
     href: 'https://www.sakneen.com/en',
   },
   {
-    title: 'Error Handler E2',
-    description: 'This is an open source package for nodejs error handler.',
-    imgSrc: '/static/projects/error-handler-e2.png',
-    href: 'https://miladezzat.github.io/error-handler-e2',
-  },
-  {
     title: 'Encrypt By RSA Algorithm',
     description: 'This is an open source package for encrypt.',
     imgSrc: '/static/projects/error-handler-e2.png',
     href: 'https://miladezzat.github.io/encrypt-rsa',
+  },
+  {
+    title: 'Error Handler E2',
+    description: 'This is an open source package for nodejs error handler.',
+    imgSrc: '/static/projects/error-handler-e2.png',
+    href: 'https://miladezzat.github.io/error-handler-e2',
   },
   {
     title: 'Memoz',
@@ -29,18 +29,6 @@ const projectsData = [
       'Time-based One-Time Password: Time-based One-time Password is a computer algorithm that generates a one-time password which uses the current time as a source of uniqueness',
     imgSrc: '/static/projects/totp.jpeg',
     href: 'https://60-min-code.github.io/gen-totp',
-  },
-  {
-    title: 'Image Processing Service',
-    description: 'This website, compression, and processes images and convert their format live',
-    imgSrc: '/static/projects/image-proccessing.png',
-    href: 'https://images-process.herokuapp.com/',
-  },
-  {
-    title: 'Sakneen Master Plan Tool',
-    description: 'The cool project built on top of map box',
-    imgSrc: '/static/projects/sakneen-master-plan-tool.png',
-    href: 'https://sakneen-masterplan-tool.herokuapp.com/',
   },
   {
     title: 'The App by Babyation®',
@@ -129,7 +117,7 @@ const projectsData = [
     description:
       "This app is a time tracking app, that helps you to track your time and your team time. it's multi-tenant app, and it's free for now.",
     imgSrc: '/static/projects/time-cards.png',
-    href: 'https://dasix.time.cards',
+    href: 'https://time.cards',
   },
   {
     title: 'PuraVida Bitcoin App on Google Play',

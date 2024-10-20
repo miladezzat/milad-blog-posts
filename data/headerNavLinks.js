68214f513cc27ef4f4d6f6a8863876c92fb7ba30
Milad E. Fahmy <miladezzat.f@gmail.com>
2024-10-20 10:43:52 +0300

    add route

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -2,6 +2,7 @@ const headerNavLinks = [
   { href: '/blog', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
   { href: '/projects', title: 'Projects' },
+  { href: '/open-source-projects', title: 'Open-Source Projects' },
   { href: '/books', title: 'Books' },
   { href: '/courses', title: 'Courses' },
   { href: '/about', title: 'About' },
